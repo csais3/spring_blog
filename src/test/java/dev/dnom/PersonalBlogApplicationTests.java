5c7467175df9fa0ea9ccddf3cbe77def3b13a632
dnom <c.sais1986@gmail.com>
2024-12-30 13:22:45 -0700

    first commit

---
+++ src/test/java/dev/dnom/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dnom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
