0c7bc1effd9e0097f398d9102bfadf5ffd810a82
dnom <c.sais1986@gmail.com>
2024-12-31 08:24:51 -0700

    added again

--- src/main/java/dev/dnom/services/CommentService.java
+++ src/main/java/dev/dnom/services/CommentService.java
@@ -1,4 +1,8 @@
 package dev.dnom.services;
 
+import dev.dnom.domain.dto.CommentDto;
+
 public interface CommentService {
+
+    CommentDto createComment(CommentDto comment);
 }
--- src/main/java/dev/dnom/services/impl/CommentServiceImpl.java
+++ src/main/java/dev/dnom/services/impl/CommentServiceImpl.java
@@ -1,7 +1,21 @@
 package dev.dnom.services.impl;
 
+import dev.dnom.domain.dto.CommentDto;
+import dev.dnom.repositories.CommentRepository;
+import dev.dnom.services.CommentService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CommentServiceImpl {
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public CommentDto createComment(CommentDto comment) {
+        return null;
+    }
 }
